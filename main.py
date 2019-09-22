8494c4e7cdec367ef8361dd12c836bfb202ca41a
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-09-22 14:21:50 +0900

    Initial Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+def search(keyword):
+  headers = {
+    'User-Agent': (
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+      'AppleWebKit/537.36 (KHTML, like Gecko) '
+      'Chrome/39.0.2171.95 '
+      'Safari/537.36'
+    )
+  }
+  res = requests.get(f'https://www.onoffmix.com/event/main?s={keyword}', headers=headers)
+
+  html = BeautifulSoup(res.text, 'html.parser')
+  events = html.find_all('article', class_='event_area')
+  events = [{
+    'id': e.find('a')['href'][-6:],
+    'title': e.find('h5', class_='title')['title'],
+    'date': e.find('div', class_='date').text,
+  } for e in events]
+
+  return events
+
+events = search('해커톤') + search('아이디어톤')
+print(*events, sep='\n')
